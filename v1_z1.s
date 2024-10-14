188e2b32624e20d5fc4e823258d89c60b27fa7c2
mihajlohadzic <79867840+mihajlohadzic@users.noreply.github.com>
2024-10-14 19:57:16 +0200

    Add files via upload

---
+++ v1_z1.s
@@ -0,0 +1,39 @@
+li t0, 2
+li t1, 4
+li t2, 5
+
+add s0, t0, t1
+sub s0, s0, t2
+
+addi s1, t0, 1
+mul t3, t1, t2 
+add s1, s1,t3
+
+li a0, 1
+sll s2, t0, t1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
